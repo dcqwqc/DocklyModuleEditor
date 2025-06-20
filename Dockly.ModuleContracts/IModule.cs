07c47c26c8bfbc8948a7bb11193d582f5711f4e0
qwqc <lz@live.de>
2025-06-20 17:58:23 +0200

    Initial commit

---
+++ Dockly.ModuleContracts/IModule.cs
@@ -0,0 +1,11 @@
+﻿namespace Dockly.ModuleContracts
+{
+    public interface IModule
+    {
+        string ModuleName { get; }
+        string ModuleVersion { get; }
+        
+        int PreferredTileWidth => 1;
+        int PreferredTileHeight => 1;
+    }
+}
---
+++ RunModule/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RunModule;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
